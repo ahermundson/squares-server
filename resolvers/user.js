7002339a901dfb3dc670a3fc252702f7de39cb8b
Alex Hermundson <alex.hermundson@gmail.com>
2020-01-28 19:58:14 -0600

    Auth

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,3 +1,5 @@
+import { tryLogin } from "../auth";
+
 export default {
   Mutation: {
     createUser: async (_, __, { models }) => {
@@ -9,6 +11,22 @@ export default {
         username: "ahermundson"
       });
       return newUser;
+    },
+    login: (_, { email, password }, { models, SECRET, SECRET2 }) =>
+      tryLogin(email, password, models, SECRET, SECRET2),
+    register: async (_, args, { models }) => {
+      try {
+        const user = await models.User.create(args);
+        return {
+          ok: true,
+          user
+        };
+      } catch (err) {
+        return {
+          ok: false,
+          errors: "Something went wrong"
+        };
+      }
     }
   }
 };
